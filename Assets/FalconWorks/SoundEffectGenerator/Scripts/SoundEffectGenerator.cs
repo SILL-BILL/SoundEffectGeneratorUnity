c2ea821fbe183f47b791b6800c3e2df29855d03b
SILL-BILL <7th5jack@gmail.com>
2018-03-03 15:03:30 +0900

    0.1.0

---
+++ Assets/FalconWorks/SoundEffectGenerator/Scripts/SoundEffectGenerator.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectGenerator : MonoBehaviour {
+
+	// --------
+	#region インスペクタ設定用フィールド
+	/// <summary>
+	/// The pet prefab table.
+	/// </summary>
+	[SerializeField] private SoundEffectPrefabTable soundEffectPrefabTable;
+	#endregion
+
+	// --------
+	#region メンバフィールド
+	/// <summary>
+	/// The pet prefabs.
+	/// </summary>
+	private Dictionary<string, GameObject> soundEffectPrefabs;
+	#endregion
+
+	// --------
+	#region MonoBehaviourメソッド
+	/// <summary> 
+	/// 初期化処理
+	/// </summary>
+	void Awake() {
+		soundEffectPrefabs = soundEffectPrefabTable.GetTable ();
+	}
+	/// <summary> 
+	/// 開始処理
+	/// </summary>
+	void Start () {
+
+	}
+	/// <summary> 
+	/// 更新処理
+	/// </summary>
+	void Update () {
+
+	}
+
+	/// <summary> 
+	/// 更新処理
+	/// </summary>
+	void LateUpdate(){
+
+	}
+	#endregion
+
+	// --------
+	#region メンバメソッド
+	/// <summary>
+	/// Popin the SoundEffect.
+	/// </summary>
+	/// <param name="_keyName">Key name.</param>
+	public void popin(string _keyName){
+
+		if (!string.IsNullOrEmpty(_keyName) && soundEffectPrefabs.ContainsKey(_keyName)) {
+
+			Instantiate (
+				soundEffectPrefabs[_keyName],
+				this.transform.position,
+				Quaternion.identity,
+				this.transform
+			);
+		}
+	}
+	#endregion
+
+	// --------
+	#region インナークラス
+	/// <summary>
+	/// ジェネリックを隠すために継承してしまう
+	/// [System.Serializable]を書くのを忘れない
+	/// </summary>
+	[System.Serializable]
+	public class SoundEffectPrefabTable : Serialize.TableBase<string, GameObject, SoundEffectPrefabPair>{
+
+	}
+	/// <summary>
+	/// ジェネリックを隠すために継承してしまう
+	/// [System.Serializable]を書くのを忘れない
+	/// </summary>
+	[System.Serializable]
+	public class SoundEffectPrefabPair : Serialize.KeyAndValue<string, GameObject>{
+		public SoundEffectPrefabPair (string key, GameObject value) : base (key, value) {
+		}
+	}
+	#endregion
+}
---
+++ Assets/FalconWorks/SoundEffectGenerator/Scripts/_Util/AutoDestoryAudio.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestoryAudio : MonoBehaviour {
+
+	// --------
+	#region インスペクタ設定用フィールド
+	#endregion
+
+	// --------
+	#region メンバフィールド
+	/// <summary> 
+	/// オーディオソース
+	/// </summary>
+	private AudioSource audio;
+	private float endTime;
+	#endregion
+
+	// --------
+	#region MonoBehaviourメソッド
+	/// <summary> 
+	/// 初期化処理
+	/// </summary>
+	void Awake() {
+
+	}
+	/// <summary> 
+	/// 開始処理
+	/// </summary>
+	void Start () {
+		audio = this.GetComponent<AudioSource>();
+		endTime = audio.clip.length;
+		Debug.Log (endTime);
+		Destroy (this.gameObject, endTime);
+	}
+	/// <summary> 
+	/// 更新処理
+	/// </summary>
+	void Update () {
+
+	}
+
+	/// <summary> 
+	/// 更新処理
+	/// </summary>
+	void LateUpdate(){
+
+	}
+	#endregion
+
+	// --------
+	#region メンバメソッド
+	#endregion
+
+	// --------
+	#region インナークラス
+	#endregion
+
+}
